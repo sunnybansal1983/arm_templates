ee46c7a3f7a67fc32c4f8e269c68fe63820622bc
sunnybansal1983 <sunnybansal1983@gmail.com>
2021-06-14 22:16:24 +1000

    commit

--- ubuntu_atom_setup.sh
+++ ubuntu_atom_setup.sh
@@ -1,8 +1,8 @@
 sudo apt-get update
 sudo su root
-mkdir -p /opt/apps/boomi/atom
-sudo chown -R boomiuser:boomiuser /opt/apps
+sudo chown -R boomiuser:boomiuser /opt/
 sudo su boomiuser
+mkdir -p /opt/apps/boomi/atom
 cd /opt/apps/boomi/atom
 wget https://platform.boomi.com/atom/atom_install64.sh
 sh ./atom_install64.sh -q -console -VinstallToken=atom-9db86db1-13c9-47a9-8fc5-c054e5954e2a -VatomName=myAtom1234  -dir /opt/apps/boomi/atom
