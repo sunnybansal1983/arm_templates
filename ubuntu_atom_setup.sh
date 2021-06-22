9228844d1d659522935702ae5561aa20fb580bbd
sunnybansal1983 <sunnybansal1983@gmail.com>
2021-06-22 23:22:23 +1000

    commit

--- ubuntu_atom_setup.sh
+++ ubuntu_atom_setup.sh
@@ -2,4 +2,4 @@ mkdir -p /opt/apps/boomi/intg
 sudo chown -R boomiuser:boomiuser /opt/apps/boomi
 su - boomiuser -c "wget -O /opt/apps/boomi/intg/atom_install64.sh https://platform.boomi.com/atom/atom_install64.sh"
 chmod +x /opt/apps/boomi/intg/atom_install64.sh
-su - boomiuser -c "/opt/apps/boomi/intg/atom_install64.sh -q -console -VinstallToken=atom-bc574b41-2618-49e2-badf-6f3a37d32a27 -VatomName=myAtom1234  -dir /opt/apps/boomi/intg"
+su - boomiuser -c "/opt/apps/boomi/intg/atom_install64.sh -q -console -VinstallToken=atom-5726ec66-1744-403a-98e1-a28a1466b726 -VatomName=myAtom12345  -dir /opt/apps/boomi/intg"
